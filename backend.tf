d96797045da3977bc0f6ec0acea22bd19427eed4
unknown <hariprasathravi98@gmail.com>
2022-07-26 14:25:26 +0530

    tf init

--- backend.tf
+++ backend.tf
@@ -2,12 +2,12 @@ terraform {
   backend "gcs" {
     bucket      = "poc-cross-access-state"
     prefix      = "terraform/state"
-    credentials = "gcp-learn-354506-8cb68555374d.json"
+    # credentials = "gcp-learn-354506-8cb68555374d.json"
   }
 }
 
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file("gcp-learn-354506-8cb68555374d.json")
+  # credentials = file("gcp-learn-354506-8cb68555374d.json")
 }
